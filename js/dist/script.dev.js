b7d4a064e6e2cad9a7fd35232855f224f4a257e7
Jikobouei-capital <adrague.ggoch@gmail.com>
2020-08-25 17:56:24 +0900

    website low quarity www lol

---
+++ js/dist/script.dev.js
@@ -0,0 +1,21 @@
+"use strict";
+
+function tab1() {
+  window.open('file/kikaku.pdf');
+}
+
+function tab2() {
+  window.open('file/siyou.pdf');
+}
+
+function tab3() {
+  window.open('file/postar.pdf');
+}
+
+function tab4() {
+  window.open('file/report.pdf');
+}
+
+function tab5() {
+  window.open('member.html');
+}
---
+++ js/script.js
@@ -0,0 +1,19 @@
+function tab1() {
+     window.open('file/kikaku.pdf');
+}
+
+function tab2() {
+     window.open('file/siyou.pdf');
+}
+
+function tab3() {
+     window.open('file/postar.pdf');
+}
+
+function tab4() {
+     window.open('file/report.pdf');
+}
+
+function tab5() {
+     window.open('member.html');
+}
